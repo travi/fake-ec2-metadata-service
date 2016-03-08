c2fb02bb4c57d798d5751d6765e3e5bbe0aba1ed
Matt Travi <matt@dwolla.com>
2016-03-08 16:01:15 -0600

    added wildcard route for security-credentials that reads from ~/.aws/credentials

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'json'
+gem 'inifile', '~> 2.0.2'
--- ec2-metadata-service.rb
+++ ec2-metadata-service.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'socket'
 require 'ipaddr'
+require 'json'
+require 'inifile'
 
 set :bind, '169.254.169.254'
 set :port, '80'
@@ -17,4 +19,16 @@ end
 
 get '/latest/meta-data/local-hostname' do
     `hostname`
+end
+
+get '/latest/meta-data/iam/security-credentials/*' do
+    content_type :json
+
+    creds_file = IniFile.load('/tmp/.aws/credentials').to_h
+
+    {
+        :Code => 'Success',
+        :AccessKeyId => creds_file['default']['aws_access_key_id'],
+        :SecretAccessKey => creds_file['default']['aws_secret_access_key']
+    }.to_json
 end
